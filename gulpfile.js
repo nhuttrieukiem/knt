754ee25c1f24b75ca32ebfe4d3144e77aa3d027b
nhuttrieukiem <trieu1810@gmail.com>
2017-03-07 22:40:21 +0700

    Build styls and watch tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,41 @@
 /* File: gulpfile.js */
 
 // grab our gulp packages
-var gulp        = require('gulp'),
-    gutil       = require('gulp-util'),
-    imagemin    = require('gulp-imagemin'),
-    del         = require('del'),
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    imagemin = require('gulp-imagemin'),
+    del = require('del'),
     runSequence = require('run-sequence'),
     consolidate = require('gulp-consolidate'),
-    iconfont    = require('gulp-iconfont'),
-    watch       = require('gulp-watch'),
-    sequence    = require('gulp-watch-sequence'),
-    gulpMerge   = require('gulp-merge'),
-    exec        = require('child_process').exec,
-    concat      = require('gulp-concat');
+    iconfont = require('gulp-iconfont'),
+    watch = require('gulp-watch'),
+    sequence = require('gulp-watch-sequence'),
+    gulpMerge = require('gulp-merge'),
+    exec = require('child_process').exec,
+    concat = require('gulp-concat'),
+    data = require('gulp-data'),
+    stylus = require('gulp-stylus'),
+    sourcemaps = require('gulp-sourcemaps');
 
 // define paths
 var paths = {
-    dist             : 'dist/*',
-    src              : 'src',
-    iconSource       : 'src/icons/*.svg',
-    iconDist         : 'dist/icons/',
-    imagesSource     : 'src/images/*',
-    imagesDist       : 'dist/images/',
-    imagesDistClean  : 'dist/images/*',
-    scriptLibDist    : 'dist/js/',
-    templateSource   : 'src/js/*/templates/**/*',
-    templateDist     : 'dist/views/',
-    scriptCommon     : 'src/js/common/*/*js',
-    scriptApp        : 'src/js/app.js',
-    scriptPages      : ['src/js/sample/*js'],
-    fontSource       : 'src/fonts/*/*',
-    fontDist         : 'dist/fonts/',
+    dist: 'dist/*',
+    src: 'src',
+    iconSource: 'src/icons/*.svg',
+    iconDist: 'dist/icons/',
+    imagesSource: 'src/images/*',
+    imagesDist: 'dist/images/',
+    imagesDistClean: 'dist/images/*',
+    scriptLibDist: 'dist/js/',
+    templateSource: 'src/js/*/templates/**/*',
+    templateDist: 'dist/views/',
+    scriptCommon: 'src/js/common/*/*js',
+    scriptApp: 'src/js/app.js',
+    scriptPages: ['src/js/sample/*js'],
+    fontSource: 'src/fonts/*/*',
+    fontDist: 'dist/fonts/',
+    styleSource: 'src/style/',
+    styleDist: 'dist/css/'
 };
 
 // library paths
@@ -56,103 +61,129 @@ var kui_common = [
 ];
 
 // create default gulp task 
-gulp.task('default', function(callback){
+gulp.task('default', function (callback) {
     runSequence('clean', 'image-compress', 'copy-views',
-                'build-css-lib', 'copy-fonts',
-                'iconfont', 'build-js-lib',
-                'build-js-kui', 'build-js-common','build-js-pages','copy-master-page',
-                'watch', callback);
+        'build-css-lib', 'build-css-pages', 'copy-fonts',
+        'iconfont', 'build-js-lib',
+        'build-js-kui', 'build-js-common', 'build-js-pages', 'copy-master-page',
+        'watch', callback);
 });
 
 // image compression
-gulp.task('image-compress', function() {
+gulp.task('image-compress', function () {
     gulp.src(paths.imagesSource)
         .pipe(imagemin())
         .pipe(gulp.dest(paths.imagesDist));
 });
 
 // clean all content dist folder
-gulp.task('clean', function() {
-  return del(paths.dist);
+gulp.task('clean', function () {
+    return del(paths.dist);
 });
 
 // clean all content of dist/images
-gulp.task('clean-images', function() {
-  return del(paths.imagesDist + "*");
+gulp.task('clean-images', function () {
+    return del(paths.imagesDist + "*");
 });
 
 // clean all content of dist/icons
-gulp.task('clean-icons', function() {
+gulp.task('clean-icons', function () {
     return del(paths.iconDist + "*");
 });
 
 // clean all content of dist/js
-gulp.task('clean-js', function() {
+gulp.task('clean-js', function () {
     return del(paths.scriptLibDist + "*");
 });
 
 
 // clean views 
-gulp.task('clean-views', function() {
+gulp.task('clean-views', function () {
     return del(paths.templateDist + "*");
 });
 
 // clean fonts
-gulp.task('clean-fonts', function() {
+gulp.task('clean-fonts', function () {
     return del(paths.fontDist + "*");
+});
+
+// clean js common
+gulp.task('clean-js-common', function () {
+    return del('dist/js/k-ui-common.js');
+});
+
+// clean js k-ui
+gulp.task('clean-js-kui', function () {
+    return del('dist/js/k-ui.js');
+});
+
+// clean js pages
+gulp.task('clean-js-pages', function () {
+    return del('dist/js/k-ui-pages.js');
+});
+
+// clean master page
+gulp.task('clean-master-page', function () {
+    return del('dist/index.html');
 });
 
 // watch 
 gulp.task('watch', function () {
-  var queue = sequence(300);
+    var queue = sequence(300);
 
-  //watch images
-  watch(paths.imagesSource, {
-    name      : 'IMAGES',
-    emitOnGlob: false
-  }, queue.getHandler('clean-images', 'image-compress'));
+    //watch images
+    watch(paths.imagesSource, {
+        name: 'IMAGES',
+        emitOnGlob: false
+    }, queue.getHandler('clean-images', 'image-compress'));
 
-  //watch icons
-  watch(paths.iconSource, {
-    name      : 'ICONS',
-    emitOnGlob: false
-  }, queue.getHandler('clean-icons', 'iconfont'));
+    //watch icons
+    watch(paths.iconSource, {
+        name: 'ICONS',
+        emitOnGlob: false
+    }, queue.getHandler('clean-icons', 'iconfont'));
 
-  //wath master page
-   watch("src/index.html", {
-    name      : 'MASTER PAGE',
-    emitOnGlob: false
-  }, queue.getHandler('copy-master-page'));
+    //wath master page
+    watch("src/index.html", {
+        name: 'MASTER PAGE',
+        emitOnGlob: false
+    }, queue.getHandler('clean-master-page', 'copy-master-page'));
 
-  //watch js common
-   watch(paths.scriptCommon, {
-    name      : 'JS COMMON',
-    emitOnGlob: false
-  }, queue.getHandler('build-js-common'));
+    //watch js common
+    watch(paths.scriptCommon, {
+        name: 'JS COMMON',
+        emitOnGlob: false
+    }, queue.getHandler('clean-js-common', 'build-js-common'));
 
-  //watch app.js
-   watch(paths.scriptApp, {
-    name      : 'APP.JS',
-    emitOnGlob: false
-  }, queue.getHandler('build-js-kui'));
+    //watch app.js
+    watch(paths.scriptApp, {
+        name: 'APP.JS',
+        emitOnGlob: false
+    }, queue.getHandler('clean-js-kui', 'build-js-kui'));
 
-  //watch page js
-   watch(paths.scriptPages, {
-    name      : 'JS PAGES',
-    emitOnGlob: false
-  }, queue.getHandler('build-js-pages'));
+    //watch page js
+    watch(paths.scriptPages, {
+        name: 'JS PAGES',
+        emitOnGlob: false
+    }, queue.getHandler('clean-js-pages', 'build-js-pages'));
 
-  //watch views
-  watch(paths.templateSource, {
-    name      : 'VIEWS',
-    emitOnGlob: false
-  }, queue.getHandler('clean-views', 'copy-views'));
+    //watch views
+    watch(paths.templateSource, {
+        name: 'VIEWS',
+        emitOnGlob: false
+    }, queue.getHandler('clean-views', 'copy-views'));
 
-  //watch fonts
-   watch(paths.fontSource, {
-    name      : 'FONTS',
-    emitOnGlob: false
-   }, queue.getHandler('clean-fonts', 'copy-fonts'));
+    //watch fonts
+    watch(paths.fontSource, {
+        name: 'FONTS',
+        emitOnGlob: false
+    }, queue.getHandler('clean-fonts', 'copy-fonts'));
+
+    //watch css pages
+    watch(paths.styleSource + 'pages', {
+        name: 'CSS_PAGES',
+        emitOnGlob: false
+    }, queue.getHandler('clean-css-pages', 'build-css-pages'));
 });
 
 // build iconfont
@@ -182,55 +213,60 @@ gulp.task('iconfont', function () {
                 }))
                 .pipe(gulp.dest(paths.iconDist));
         })
-        .pipe(gulp.dest(paths.iconDist).on('finish', function(){
+        .pipe(gulp.dest(paths.iconDist).on('finish', function () {
             console.log("Iconfont was generated successfully!!");
         }));
 });
 
 // Build js lib
 gulp.task('build-js-lib', function () {
-  return gulpMerge(
-     gulp.src(lib)
+    return gulpMerge(
+        gulp.src(lib)
     )
-    .pipe(concat('k-ui-lib.js'))
-    .pipe(gulp.dest('dist/js/'));
+        .pipe(concat('k-ui-lib.js'))
+        .pipe(gulp.dest('dist/js/'));
 });
 
 // Build k-ui js
 gulp.task('build-js-kui', function () {
-  return gulpMerge(
-     gulp.src(kui)
+    return gulpMerge(
+        gulp.src(kui)
     )
-    .pipe(concat('k-ui.js'))
-    .pipe(gulp.dest('dist/js/'));
+        .pipe(concat('k-ui.js'))
+        .pipe(gulp.dest('dist/js/'));
 });
 
 // Build k-ui js
 gulp.task('build-js-common', function () {
-  return gulpMerge(
-     gulp.src(kui_common)
+    return gulpMerge(
+        gulp.src(kui_common)
     )
-    .pipe(concat('k-ui-common.js'))
-    .pipe(gulp.dest('dist/js/'));
+        .pipe(concat('k-ui-common.js'))
+        .pipe(gulp.dest('dist/js/'));
 });
 
 // Build js pages
 gulp.task('build-js-pages', function () {
-  return gulpMerge(
-     gulp.src(paths.scriptPages)
+    return gulpMerge(
+        gulp.src(paths.scriptPages)
     )
-    .pipe(concat('k-ui-pages.js'))
-    .pipe(gulp.dest('dist/js/'));
+        .pipe(concat('k-ui-pages.js'))
+        .pipe(gulp.dest('dist/js/'));
 });
 
 
 // Build css lib
 gulp.task('build-css-lib', function () {
     return gulpMerge(
-     gulp.src(css_lib)
+        gulp.src(css_lib)
     )
-    .pipe(concat('k-ui-lib.css'))
-    .pipe(gulp.dest('dist/css/'));
+        .pipe(concat('k-ui-lib.css'))
+        .pipe(gulp.dest(paths.styleDist));
+});
+
+// clean css lib
+gulp.task('clean-css-lib', function () {
+    return del(paths.styleDist + 'k-ui-lib.css');
 });
 
 // Copy master page
@@ -242,20 +278,33 @@ gulp.task('copy-master-page', function () {
 //copy fonts
 gulp.task('copy-fonts', function () {
     gulp.src(paths.fontSource)
-    .pipe(gulp.dest(paths.fontDist));
+        .pipe(gulp.dest(paths.fontDist));
 });
 
 // copy views
 gulp.task('copy-views', function () {
     gulp.src(paths.templateSource)
-    .pipe(gulp.dest(paths.templateDist));
+        .pipe(gulp.dest(paths.templateDist));
 });
 
 gulp.task('start-server', function (cb) {
-  exec('lite-server -c lite-server.js', function (err, stdout, stderr) {
-    console.log(stdout);
-    console.log(stderr);
-    cb(err);
-  });
+    exec('lite-server -c lite-server.js', function (err, stdout, stderr) {
+        console.log(stdout);
+        console.log(stderr);
+        cb(err);
+    });
+});
+
+// build css pages
+gulp.task('build-css-pages', function () {
+    return gulp.src(paths.styleSource + 'pages/*.styl')
+        .pipe(stylus())
+        .pipe(concat('k-ui-pages.css'))
+        .pipe(gulp.dest(paths.styleDist));
+});
+
+// clean css pages
+gulp.task('clean-css-pages', function () {
+    return del(paths.styleDist + 'k-ui-pages.css');
 });
 
